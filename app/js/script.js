0fd138a56e22cf6cf9ae878403da27908ba87bf0
Nicolay Poymanov <n.poymanov@gmail.com>
2015-07-31 00:56:28 +0400

    Вторая часть задания по сайту-портфолио

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,47 @@
-$(function() {
+var app = (function() {
 
-	$('#new-work').bind('click', function(e) {
-		e.preventDefault();
-		newPop = $('#block-upload').bPopup();
-	});
+	//Инициализация модуля приложения
+	var init = function() {
+		_setUpListeners();
+		_repairPlaceholder();
+	};
 
-	$('#close-form').bind('click', function(e) {
-		e.preventDefault();
-		newPop.close();
-	});
+	//Прослушка событий приложения
+	var _setUpListeners = function() {
+		//Вызов попапа
+		$('#new-work').bind('click', function(e) {
+			e.preventDefault();
+			newPop = $('#block-upload').bPopup({
+				transition: "slideDown"
+			});
+		});
 
-});
+		//Выбор файла-изображения
+		$('#form-file').change(function() {
+			$(this).each(function() {
+				var name = this.value;
+				reWin = /.*\\(.*)/;
+				var fileTitle = name.replace(reWin, "$1");
+				reUnix = /.*\/(.*)/;
+				fileTitle = fileTitle.replace(reUnix, "$1");
+				$('.fake-input').attr('placeholder', fileTitle);
+				$('.fake-input').attr('value', fileTitle);
+			});
+		});
+	};
+
+	//Исправление placeholder в IE
+	var _repairPlaceholder = function(){
+		if(!Modernizr.input.placeholder){
+			$('input, textarea').placeholder();
+		}
+	}
+
+	return {
+		init: init
+	};
+
+})();
+
+
+app.init();
