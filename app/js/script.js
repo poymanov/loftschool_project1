a29cb03366a555512224a3fa3ff195c73c25d7de
Nicolay Poymanov <n.poymanov@gmail.com>
2015-08-02 16:52:34 +0400

    Первая часть по заданию 3

--- app/js/script.js
+++ app/js/script.js
@@ -6,6 +6,99 @@ var app = (function() {
 		_repairPlaceholder();
 	};
 
+	//Показать тултип
+	var showQtip = function (id) {
+		var el = $('#' + id),
+			pos = el.attr('qtip-position');
+			_createTooltip(el,pos);
+	}
+
+	//Создание тултипа
+	var _createTooltip = function(el,position){
+		if (position === 'right') {
+			position = {
+				my: 'left center',
+				at: 'right center'
+			};		
+		}
+		else {
+			position = {
+				my: 'right center',
+				at: 'left center'
+			};
+		}
+
+		el.qtip({
+			content: {
+				text: function(){
+					return $(this).attr('qtip-content')
+				}
+			},
+			show: {
+				event: 'show'
+			},
+			hide: {
+				event: 'keydown hideTooltip'
+			},
+			position: position,
+			style: {
+				classes: 'qtip-red qtip-rounded'
+			}
+		}).trigger('show');
+	};
+
+	//Универсальная процедура ajax-submit формы
+	var _submitForm = function(curForm,file,parentBlock){
+		$('#'+curForm).on('submit',function(e){
+			e.preventDefault();
+
+			var isError = false;
+			//Проверка заполнения формы (берем только поля нужные для валидации)
+			$('#'+curForm+' input:not(".no-validation"), textarea').each(function(index, el) {
+				if($(el).val().length === 0) {
+					showQtip($(this).attr('id'));
+					isError = true;
+				}
+			});
+
+			//Если возникли ошибки заполнения формы, то дальше процедура не идет
+
+			if (isError) {
+				return;
+			}
+
+			//Очищаем сообщение об ошибках формы
+			_resetFormsErrors();
+
+			var data = $(this).serialize(),
+				url = '/app/'+file+'.php';
+
+
+			$.ajax({
+				url: url,
+				type: 'post',
+				dataType: 'json',
+				data: data,
+			})
+			.fail(function(ans) {
+				$('#'+parentBlock+' .msg-error .msg-error-text').text('На сервере произошла ошибка');
+				$('#'+parentBlock+ ' .msg-error').show();
+			})
+			.done(function(ans) {
+				//если все поля заполнены, сообщяем об этом пользователю
+				if (ans.status === "OK") {
+					$('.msg-success').bPopup();
+				}
+				else {
+					//Выводим сообщение об ошибке в специальное поле на форме
+					$('#'+parentBlock+' .msg-error .msg-error-text').text(ans.msg);
+					$('#'+parentBlock+' .msg-error').show();
+				}
+				
+			})
+		})
+	};
+
 	//Прослушка событий приложения
 	var _setUpListeners = function() {
 		//Вызов попапа
@@ -13,7 +106,7 @@ var app = (function() {
 			e.preventDefault();
 			newPop = $('#block-upload').bPopup({
 				transition: "slideDown",
-				onClose: _resetFormUpload
+				onClose: _onClosePopup
 			});
 		});
 
@@ -27,36 +120,22 @@ var app = (function() {
 				fileTitle = fileTitle.replace(reUnix, "$1");
 				$('.fake-input').attr('placeholder', fileTitle);
 				$('.fake-input').attr('value', fileTitle);
+				$('.fake-input').trigger('hideTooltip');
 			});
 		});
 
-		$('form').on('submit',function(e){
+		//Закрытие окна сообщения с ошибкой внутри формы
+		$('.msg-error-close').on('click', function(e) {
 			e.preventDefault();
+			$(this).parent().hide();
+		});
 
-			var data = $(this).serialize();
+		//Форма добавления работы
+		_submitForm('form-upload','server','block-upload');
 
-			$.ajax({
-				url: '/app/server.php',
-				type: 'post',
-				dataType: 'json',
-				data: data,
-			})
-			.fail(function(ans) {
-				$('#block-upload .msg-error').text('На сервере произошла ошибка').show();
-			})
-			.done(function(ans) {
-				//если все поля заполнены, сообщяем об этом пользователю
-				if (ans.status === "OK") {
-					alert('Новая работа успешно добавлена');
-				}
-				else {
-					//Выводим сообщение об ошибке в специальное поле на форме
-					$('#block-upload .msg-error').text(ans.msg).show();
-				}
-				
-			})
-			
-		})
+		//Форма обратной связи
+		_submitForm('feedback-form','server','feedback');
+
 	};
 
 	//Исправление placeholder в IE
@@ -66,16 +145,42 @@ var app = (function() {
 		}
 	};
 
-	//Сбрасываем все ошибки и значения формы
-	var _resetFormUpload = function(){
-		$('#block-upload .msg-error').text('').hide();
+	//Сбрасываем все ошибки формы
+	var _resetFormsErrors = function(){
+		$('#block-upload .msg-error .msg-error-text').text('');
+		$('#block-upload .msg-error').hide();
+	};
+
+	//Убираем все тултипы
+	var _hideTooltips = function(){
+		$('input').trigger('hideTooltip');
+		$('textarea').trigger('hideTooltip');
 	}
 
+	//Процедуры после закрытия формы
+	var _onClosePopup = function() {
+		//Сбрасываем все ошибки и значения формы
+		_resetFormsErrors();
+
+		//Убираем все тултипы
+		_hideTooltips();
+	}
 	return {
-		init: init
+		init: init,
+		showQtip: showQtip
 	};
 
 })();
 
 
 app.init();
+
+
+
+
+// Прячем тултипы по одному
+		// $('#name').trigger('hideTooltip');
+		// $('#email').trigger('hideTooltip');
+
+		// Прячем все тултипы сразу 
+		// $('.input').trigger('hideTooltip');
